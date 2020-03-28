3d1ba63ae7e16ba31ade78f0704d8c4043b03248
keshav aggarwal <aggarwalkeshav38@gmail.com>
2020-03-28 19:36:00 +0530

    user data and find of user request added

--- index.js
+++ index.js
@@ -4,20 +4,33 @@ const port = 8080
 
 const fs = require('fs');
 
-let rawdata = fs.readFileSync('quotes.json');  
-let quotes = JSON.parse(rawdata);  
+let rawdata = fs.readFileSync('users.json');  
+let users = JSON.parse(rawdata);
 
-app.get('/quote', (req, res) => res.send(quotes[Math.floor(Math.random() * 300)]))
-
-app.get('/fibo', (req, res) => res.send(""+fibo(req.query.limit)));
+app.get('/user/:id', (req, res) => {
+	const user = findUser(req.params.id)
+	if(user != null) {
+		res.send(users[foundIndex])
+	} else {
+		res.send("No Index found")
+	}
+});
 
 app.use(express.static('public'))
 
 app.listen(port, () => console.log(`NAGP-quotes app listening on port ${port}!`))
 
+function findUser(userId) {
+	const userIdJsonKey = 'userId'
 
- function fibo(n) { 
-    if (n < 2)
-        return 1;
-    else   return fibo(n - 2) + fibo(n - 1);
+	for(const index in users) {
+		const user = users[index]
+		if(user.hasOwnProperty(userIdJsonKey)) {
+			if(user[userIdJsonKey] == userId) {
+				return user
+			}
+		}
+	}
+
+	return null
 }
